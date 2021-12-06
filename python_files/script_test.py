7e4fd9f594a158164b1693a503e73a6dab467221
pbreslin <pbreslin@tcd.ie>
2021-12-06 22:49:04 +0100

    alice tests

---
+++ python_files/script_test.py
@@ -0,0 +1,34 @@
+print('Beginning Script')
+
+# Packages
+import h5py
+from amuse.lab import units
+from evolve_model import integrate_system
+
+# Defining system parameters
+moons = ['europa']
+eccentricities = [0.2]
+inclinations = [0]
+
+# Evolving the system 
+dt = 100
+end_time = 1e4
+kdt = 360
+ecc, inc, sma, Etot, Ediss, model_time = integrate_system(moons, eccentricities, inclinations, 
+	kdt, dt, end_time)
+
+print('Model evolved, saving data')
+
+# Saving data
+filename  = 'alice_test.hdf5'
+d = h5py.File(filename, 'w')
+
+#d = results.create_group('europa')
+d['inc'] = inc[0].value_in(units.deg)
+d['ecc'] = ecc[0]
+d['sma'] = sma[0].value_in(units.m)
+d['etot'] = Etot
+d['ediss'] = Ediss
+d['time'] = model_time.value_in(units.yr)
+
+d.close()
